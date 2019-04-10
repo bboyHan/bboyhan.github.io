b45a99e3ca83bc63e00dcce8307057d1cbe38218
bboyhan <bboyhan@yeah.net>
2019-04-10 17:40:55 +0800

    update new website

--- static/js/2.60294885325f48ef774f.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2],{O3vf:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var l={render:function(){var e=this.$createElement;return(this._self._c||e)("div")},staticRenderFns:[]},r=t("VU/8")(null,l,!1,null,null,null);n.default=r.exports}});
-//# sourceMappingURL=2.60294885325f48ef774f.js.map
